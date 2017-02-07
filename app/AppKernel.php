589bae8f44f6620ce15471cca74c14f6ab18b430
nicolaspin <pin.nicolas@free.fr>
2017-02-07 23:17:28 +0100

    Start configuration of paymentsuite paypal express checkout

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new WhiteOctober\TCPDFBundle\WhiteOctoberTCPDFBundle(),
             new PaymentSuite\PaymentCoreBundle\PaymentCoreBundle(),
+            new PaymentSuite\PaypalExpressCheckoutBundle\PaypalExpressCheckoutBundle(),
             new AppBundle\AppBundle(),
         ];
 
